e210928b6efa6e28cc1f48220a6342af90e4ab5e
Rémi Roycourt <remiroycourt@gmail.com>
2023-07-18 01:13:46 +0200

    feat: handle transfers and mints

--- src/events/transfers.rs
+++ src/events/transfers.rs
@@ -2,8 +2,29 @@ use crate::starknet::{client::get_block_with_txs, utils::get_contract_property_s
 use log::info;
 use num_bigint::BigUint;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use starknet::core::types::EmittedEvent;
+use std::collections::HashMap;
 use std::error::Error;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Metadata {
+    description: String,
+    external_url: String,
+    image: String,
+    name: String,
+    attributes: Option<Vec<HashMap<String, String>>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct NormalizedMetadata {
+    description: String,
+    #[serde(rename = "external_url")]
+    initial_metadata_uri: String,
+    image: String,
+    name: String,
+    attributes: Vec<HashMap<String, String>>,
+}
 
 fn left_pad_hex_32(hex_string: String) -> String {
     let trimmed_string = hex_string.as_str().trim_start_matches("0x");
@@ -92,59 +113,161 @@ pub async fn process_transfers(client: &Client, value: &str) -> Result<(), Box<d
     let token_id_big_uint = BigUint::from_bytes_be(&bytes[..]);
     let token_id = token_id_big_uint.to_str_radix(10);
 
-    let contract_address = event.from_address.to_string().as_str();
-    // let block_number = event.block_number;
+    let contract_address = event.from_address.to_string();
+    let block_number = event.block_number;
 
-    // let contract_type = "erc721"; // TODO: Get from dynamodb
+    let token_uri = get_token_uri(
+        client,
+        token_id_low,
+        token_id_high,
+        &contract_address,
+        block_number,
+    )
+    .await;
 
-    // let token_uri = get_token_uri(
-    //     client,
-    //     token_id_low,
-    //     token_id_high,
-    //     contract_address,
-    //     block_number,
-    // )
-    // .await;
+    info!(
+        "Contract address: {} - Token ID: {} - Token URI: {} - Block number: {}",
+        contract_address, token_id, token_uri, block_number
+    );
 
-    // info!("Contract address: {} - Contract type: {} - Token ID: {} - Token URI: {} - Block number: {}",
-    //            contract_address, contract_type, token_id, token_uri, block_number
-    //        );
+    // let mint_object = serde_json::json!({
+    //     "event_type": "mint",
+    //     "transaction_hash": event.transaction_hash,
+    //     "block_hash": event.block_hash,
+    //     "from_address": from_address,
+    //     "to_address": to_address,
+    //     "contract_type": contract_attributes.contract_type,
+    //     "collection_address": contract_address,
+    //     "token_id": token_id,
+    //     "token_uri": token_uri,
+    //     "block_number": block_number,
+    //     "token_name": contract_attributes.name.to_string(),
+    //     "token_symbol": contract_attributes.symbol.to_string(),
+    //     "timestamp": timestamp.to_string()
+    // });
 
-    // if from_address == "0x0" {
-    //     let mint_object = serde_json::json!({
-    //         "event_type": "mint",
-    //         "transaction_hash": transaction_hash,
-    //         "block_hash": block_hash,
-    //         "from_address": from_address,
-    //         "to_address": to_address,
-    //         "contract_type": contract_attributes.contract_type,
-    //         "collection_address": contract_address,
-    //         "token_id": token_id,
-    //         "token_uri": token_uri,
-    //         "block_number": block_number,
-    //         "token_name": contract_attributes.name.to_string(),
-    //         "token_symbol": contract_attributes.symbol.to_string(),
-    //         "timestamp": timestamp.to_string()
-    //     });
+    // info!("Mint detected: {:?}", mint_object);
 
-    //     info!("Mint detected: {:?}", mint_object);
-
-    //     let json_event = serde_json::to_string(&mint_object).unwrap();
-
-    //     info!(
-    //         "Sending mint event to Kinesis stream: {}",
-    //         kinesis_stream.as_str()
-    //     );
-
-    //     send_to_kinesis(
-    //         kinesis_client,
-    //         kinesis_stream.as_str(),
-    //         transaction_hash,
-    //         &json_event,
-    //     )
-    //     .await
-    //     .unwrap();
-    // }
+    if from_address == "0x0" {
+        process_mint_event(client, &token_uri, timestamp).await;
+    } else {
+        // TODO
+    }
 
     Ok(())
+}
+
+async fn process_mint_event(client: &Client, token_uri: &str, timestamp: u64) {
+    let (metadata_uri, initial_metadata_uri) = sanitize_uri(token_uri).await;
+
+    // let client = DynamoDbClient::new(region::Region::UsEast1);
+    // let query_collections_input = QueryInput {
+    //     table_name: "ark_mainnet_collections".to_string(),
+    //     key_condition_expression: Some("#address = :address".to_string()),
+    //     expression_attribute_names: Some({
+    //         let mut map = HashMap::new();
+    //         map.insert("#address".to_string(), "address".to_string());
+    //         map
+    //     }),
+    //     expression_attribute_values: Some({
+    //         let mut map = HashMap::new();
+    //         map.insert(":address".to_string(), event.collection_address.into());
+    //         map
+    //     }),
+    //     limit: Some(1),
+    //     ..Default::default()
+    // };
+
+    // match client.query(query_collections_input).await {
+    //     Ok(output) => {
+    //         if let Some(items) = output.items {
+    //             if items.is_empty() {
+    //                 eprintln!("Collection not found: {}", event.collection_address);
+    //             } else {
+    //                 // TODO: Extract the collection from items and use its properties as necessary.
+    //                 //       This part highly depends on the structure of your items.
+    //                 // let collection = &items[0];
+
+    //                 // Update items as needed here...
+
+    //                 let put_item_input = PutItemInput {
+    //                     table_name: "ark_mainnet_collection_activities".to_string(),
+    //                     item: {
+    //                         let mut map = HashMap::new();
+    //                         map.insert("address".to_string(), event.collection_address.into());
+    //                         map.insert("timestamp".to_string(), timestamp.into());
+    //                         // Add remaining fields...
+    //                         map
+    //                     },
+    //                     ..Default::default()
+    //                 };
+    //                 match client.put_item(put_item_input).await {
+    //                     Ok(_) => println!("Inserting into ark_mainnet_collection_activities"),
+    //                     Err(err) => eprintln!("Error updating/inserting items: {}", err),
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     Err(err) => eprintln!("Query Error: {}", err),
+    // }
+
+    if !metadata_uri.is_empty() {
+        let metadata = fetch_metadata(client, &metadata_uri, &initial_metadata_uri)
+            .await
+            .unwrap();
+
+        // TODO: Uploading image to S3
+
+        // TODO: Inserting into ark_mainnet_tokens
+    }
+}
+
+async fn sanitize_uri(token_uri: &str) -> (String, String) {
+    let mut request_uri = token_uri
+        .trim()
+        .replace("\u{0003}", "")
+        .replace("-https://", "https://");
+    request_uri = convert_ipfs_uri_to_http_uri(request_uri);
+    (request_uri.clone(), request_uri)
+}
+
+fn convert_ipfs_uri_to_http_uri(request_uri: String) -> String {
+    let result = if request_uri.contains("ipfs://") {
+        format!(
+            "http://ec2-54-89-64-17.compute-1.amazonaws.com:8080/ipfs/{}",
+            request_uri.split("ipfs://").last().unwrap()
+        )
+    } else {
+        request_uri
+    };
+    result
+}
+
+async fn fetch_metadata(
+    client: &Client,
+    metadata_uri: &str,
+    initial_metadata_uri: &str,
+) -> Result<NormalizedMetadata, Box<dyn std::error::Error>> {
+    let res = client.get(metadata_uri).send().await?;
+    let raw_metadata: Metadata = res.json().await?;
+
+    let mut normalized_attributes = vec![];
+
+    if let Some(attributes) = raw_metadata.attributes {
+        for attribute in attributes {
+            let mut new_attribute = HashMap::new();
+            new_attribute.insert("trait_type".into(), attribute["trait_type"].clone());
+            new_attribute.insert("value".into(), attribute["value"].clone());
+            new_attribute.insert("display_type".into(), attribute["display_type"].clone());
+            normalized_attributes.push(new_attribute);
+        }
+    }
+
+    Ok(NormalizedMetadata {
+        description: raw_metadata.description,
+        initial_metadata_uri: initial_metadata_uri.to_string(),
+        image: raw_metadata.image,
+        name: raw_metadata.name,
+        attributes: normalized_attributes,
+    })
 }
--- src/utils.rs
+++ src/utils.rs
@@ -55,3 +55,32 @@ pub fn decode_long_string(array: &Vec<String>) -> Result<String, Box<dyn Error>>
 
     Ok(result)
 }
+
+async fn upload_image_to_s3(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    //     let client = reqwest::Client::new();
+    //     let res = client.get(url).send().await?;
+
+    //     let extension = url.split('.').last().unwrap_or_default();
+    //     let key = format!("{}.{}", Uuid::new_v4(), extension);
+
+    //     let s3_client = S3Client::new(Region::UsEast1);
+    //     let put_request = PutObjectRequest {
+    //         bucket: "ark-nft-images".to_string(),
+    //         key,
+    //         body: Some(ByteStream::new(res.bytes_stream())),
+    //         content_type: Some(
+    //             res.headers()
+    //                 .get("content-type")
+    //                 .unwrap()
+    //                 .to_str()
+    //                 .unwrap()
+    //                 .into(),
+    //         ),
+    //         ..Default::default()
+    //     };
+
+    //     let s3_res = s3_client.put_object(put_request).await?;
+    //     Ok(s3_res.key.unwrap())
+
+    Ok("".to_string())
+}
