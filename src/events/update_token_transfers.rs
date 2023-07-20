ac6ccd18a22dbcdad3ffd3cd37f6e9b5b126b8f0
kwiss <mr.dumont.christophe@gmail.com>
2023-07-20 16:35:23 +0200

    feat(nft-transfers): save events

---
+++ src/events/update_token_transfers.rs
@@ -0,0 +1,115 @@
+use aws_sdk_dynamodb::types::AttributeValue;
+use aws_sdk_dynamodb::Error;
+use dotenv::dotenv;
+use log::info;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::collections::HashMap;
+use std::env;
+
+#[derive(Serialize, Deserialize, Debug)]
+enum TransferType {
+    In,
+    Out,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Transfer {
+    from: String,
+    to: String,
+    kind: TransferType,
+    timestamp: String,
+    transaction_hash: String,
+}
+
+pub async fn update_token_transfers(
+    dynamo_client: &aws_sdk_dynamodb::Client,
+    contract_address: &str,
+    padded_token_id: String,
+    from_address: &str,
+    to_address: &str,
+    timestamp: &u64,
+    transaction_hash: &str,
+) -> Result<Option<String>, Error> {
+    dotenv().ok();
+    let table = env::var("ARK_TOKENS_TABLE_NAME").expect("ARK_TOKENS_TABLE_NAME must be set");
+    let contract_address_av = AttributeValue::S(contract_address.to_string());
+    let token_id_av = AttributeValue::S(padded_token_id);
+
+    // TODO read current value from dynamo
+    let request = dynamo_client
+        .get_item()
+        .table_name(&table)
+        .key("address", contract_address_av.clone())
+        .key("token_id", token_id_av.clone());
+
+    let result = request.send().await?;
+
+    let current_transfer = Transfer {
+        from: from_address.to_string(),
+        to: to_address.to_string(),
+        kind: TransferType::Out,
+        timestamp: timestamp.to_string(),
+        transaction_hash: transaction_hash.to_string(),
+    };
+
+    let current_transfer_av = convert_transfer_to_map(&current_transfer);
+
+    info!("current_transfer: {:?}", current_transfer);
+
+    match &result.item {
+        Some(item) => {
+            log::info!("item: {:?}", item);
+            if let Some(transfers_av) = item.get("transfers") {
+                let mut transfers = transfers_av.as_l().unwrap().clone();
+                transfers.push(current_transfer_av.clone());
+                let transfers_av = AttributeValue::L(transfers);
+                let request = dynamo_client
+                    .put_item()
+                    .table_name(&table)
+                    .item("address", contract_address_av.clone())
+                    .item("token_id", token_id_av.clone())
+                    .item("transfers", transfers_av)
+                    .item("token_owner", AttributeValue::S(to_address.to_string()));
+                request.send().await?;
+            }
+            // TODO update the current entry
+        }
+        None => {
+            // TODO build the body for the dynamo table save
+            let transfers_av = AttributeValue::L(vec![current_transfer_av]);
+
+            let request = dynamo_client
+                .put_item()
+                .table_name(&table)
+                .item("address", contract_address_av.clone())
+                .item("token_id", token_id_av.clone())
+                .item("transfers", transfers_av)
+                .item("token_owner", AttributeValue::S(to_address.to_string()));
+            request.send().await?;
+        }
+    }
+
+    Ok(None)
+}
+
+// Function to convert a Transfer into a Map<AttributeValue>
+fn convert_transfer_to_map(transfer: &Transfer) -> AttributeValue {
+    let mut map: HashMap<String, AttributeValue> = HashMap::new();
+    map.insert("from".into(), AttributeValue::S(transfer.from.clone()));
+    map.insert("to".into(), AttributeValue::S(transfer.to.clone()));
+    map.insert(
+        "kind".into(),
+        AttributeValue::S(serde_json::to_string(&transfer.kind).unwrap()),
+    );
+    map.insert(
+        "timestamp".into(),
+        AttributeValue::S(transfer.timestamp.clone()),
+    );
+    map.insert(
+        "transaction_hash".into(),
+        AttributeValue::S(transfer.transaction_hash.clone()),
+    );
+
+    AttributeValue::M(map)
+}
