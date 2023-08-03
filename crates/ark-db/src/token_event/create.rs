e0fda5c4b2c31f18526c3e0b38970dae095b9f82
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-08-03 18:29:57 +0200

    feat: rename table to ark_mainnet_token_events (#39)

---
+++ crates/ark-db/src/token_event/create.rs
@@ -0,0 +1,52 @@
+use aws_sdk_dynamodb::types::AttributeValue;
+use aws_sdk_dynamodb::{Client, Error};
+use log::info;
+
+pub struct TokenEvent {
+    pub address: String,
+    pub timestamp: u64,
+    pub block_number: u64,
+    pub event_type: String,
+    pub from_address: String,
+    pub padded_token_id: String,
+    pub token_uri: String,
+    pub to_address: String,
+    pub transaction_hash: String,
+    pub token_type: String,
+}
+
+pub async fn create_token_event(
+    dynamo_client: &Client,
+    token_event: TokenEvent,
+) -> Result<(), Error> {
+    info!("create_token_event: {:?}", token_event.address);
+
+    let result = dynamo_client
+        .put_item()
+        .table_name("ark_mainnet_token_events")
+        .item("address", AttributeValue::S(token_event.address))
+        .item(
+            "timestamp",
+            AttributeValue::N(token_event.timestamp.to_string()),
+        )
+        .item(
+            "block_number",
+            AttributeValue::N(token_event.block_number.to_string()),
+        )
+        .item("event_type", AttributeValue::S(token_event.event_type))
+        .item("from_address", AttributeValue::S(token_event.from_address))
+        .item("to_address", AttributeValue::S(token_event.to_address))
+        .item("token_id", AttributeValue::S(token_event.padded_token_id))
+        .item("token_uri", AttributeValue::S(token_event.token_uri))
+        .item(
+            "transaction_hash",
+            AttributeValue::S(token_event.transaction_hash),
+        )
+        .item("collection_type", AttributeValue::S(token_event.token_type))
+        .send()
+        .await;
+
+    println!("dynamodb result: {:?}", result);
+
+    Ok(())
+}
