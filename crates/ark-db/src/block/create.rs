833177f08fabff141f9371f53aa1749be55dc3c2
Rémi Roycourt <remiroycourt@gmail.com>
2023-08-08 21:32:03 +0200

    feat: add log trace

--- crates/ark-db/src/block/create.rs
+++ crates/ark-db/src/block/create.rs
@@ -4,19 +4,35 @@ use std::env;
 
 // This function adds a block number to the list of fetched blocks.
 pub async fn create_block(
-  dynamo_client: &Client,
-  block_number: u64,
-  status: bool,
+    dynamo_client: &Client,
+    block_number: u64,
+    status: bool,
 ) -> Result<(), Error> {
-  let table = env::var("ARK_BLOCKS_TABLE_NAME").expect("ARK_BLOCKS_TABLE_NAME must be set");
-  let block_number_av = AttributeValue::N(block_number.to_string());
-  let is_fetched_av = AttributeValue::Bool(status);
-  let request = dynamo_client
-      .put_item()
-      .table_name(table)
-      .item("block_number", block_number_av)
-      .item("is_fetched", is_fetched_av);
+    let table = env::var("ARK_BLOCKS_TABLE_NAME").expect("ARK_BLOCKS_TABLE_NAME must be set");
+    let block_number_av = AttributeValue::N(block_number.to_string());
+    let is_fetched_av = AttributeValue::Bool(status);
+    let request = dynamo_client
+        .put_item()
+        .table_name(table)
+        .item("block_number", block_number_av)
+        .item("is_fetched", is_fetched_av);
 
-  request.send().await?;
-  Ok(())
+    let result = request.send().await;
+
+    match result {
+        Ok(_) => {
+            println!(
+                "Successfully added block number {} to the list of fetched blocks",
+                block_number
+            );
+        }
+        Err(e) => {
+            println!(
+                "Error adding block number {} to the list of fetched blocks: {:?}",
+                block_number, e
+            );
+        }
+    }
+
+    Ok(())
 }
