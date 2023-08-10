0673d01c1cc8c59bb1a23c8ddc5bc00f6eaaa807
kwiss <mr.dumont.christophe@gmail.com>
2023-08-10 14:39:24 +0200

    feat(transfer-action): update kinesis message & lambda

--- crates/ark-stream/src/send.rs
+++ crates/ark-stream/src/send.rs
@@ -11,15 +11,18 @@ pub async fn send_to_kinesis(
     stream: &str,
     key: &str,
     data: &str,
+    contract_type: &str,
 ) -> Result<(), SdkError<PutRecordError, Response<SdkBody>>> {
     let blob = Blob::new(data);
 
-    info!("Sending data to {} stream: {}", key, data);
+    let partition_key = format!("{}:{}", contract_type, key);
+
+    info!("Sending data to {} stream: {}", partition_key, data);
 
     let request = client
         .put_record()
         .data(blob)
-        .partition_key(key)
+        .partition_key(&partition_key)
         .stream_name(stream);
 
     let result = request.send().await;
@@ -31,8 +34,6 @@ pub async fn send_to_kinesis(
         Err(err) => {
             // Log the error
             error!("Error while adding record to Kinesis stream: {:?}", err);
-
-            // Optionally, you can propagate the error further up the call stack
             Err(err)
         }
     }
--- crates/ark-transfers/src/main.rs
+++ crates/ark-transfers/src/main.rs
@@ -1,9 +1,15 @@
+mod mint;
+mod transfer;
+mod utils;
+use crate::transfer::process_transfers;
+use aws_config::meta::region::RegionProviderChain;
 use aws_lambda_events::event::kinesis::KinesisEvent;
+use aws_sdk_dynamodb::Client as DynamoClient;
 use base64;
 use base64::{engine::general_purpose, Engine as _};
 use lambda_runtime::{service_fn, Error, LambdaEvent};
-use log::info;
-use log::LevelFilter;
+use log::{info, LevelFilter};
+use reqwest::Client as ReqwestClient;
 use simple_logger::SimpleLogger;
 
 #[tokio::main]
@@ -13,12 +19,25 @@ async fn main() -> Result<(), Error> {
         .with_level(LevelFilter::Info)
         .init()
         .unwrap();
-    lambda_runtime::run(service_fn(handle_kinesis_event)).await
+
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    // Initialization: Create the necessary clients for services
+    let reqwest_client = ReqwestClient::new();
+    let dynamo_db_client = DynamoClient::new(&config);
+
+    lambda_runtime::run(service_fn(|event: LambdaEvent<KinesisEvent>| {
+        handle_kinesis_event(event, &reqwest_client, &dynamo_db_client)
+    }))
+    .await
 }
 
-async fn handle_kinesis_event(event: LambdaEvent<KinesisEvent>) -> Result<(), Error> {
+async fn handle_kinesis_event(
+    event: LambdaEvent<KinesisEvent>,
+    reqwest_client: &ReqwestClient,
+    dynamo_db_client: &DynamoClient,
+) -> Result<(), Error> {
     log::info!("Event invocation: {:?}", event);
-    // The actual Kinesis event data is in the payload field
     let kinesis_event = event.payload;
 
     for record in kinesis_event.records {
@@ -31,6 +50,31 @@ async fn handle_kinesis_event(event: LambdaEvent<KinesisEvent>) -> Result<(), Er
                     .unwrap_or_else(|_| "Invalid UTF-8 data".to_string());
                 // Log the decoded data
                 info!("Decoded data: {}", decoded_str);
+
+                // Call your process_transfers function
+                let partition_key = match &record.kinesis.partition_key {
+                    Some(key) => key,
+                    None => {
+                        info!("No partition key found in the record");
+                        continue; // Skip this iteration of the loop
+                    }
+                };
+
+                let parts: Vec<&str> = partition_key.split(':').collect();
+                let contract_type = parts.get(0).unwrap_or(&""); // or provide a default
+
+                let result = process_transfers(
+                    reqwest_client,
+                    dynamo_db_client,
+                    &decoded_str,
+                    contract_type,
+                )
+                .await;
+
+                // Handle any errors from process_transfers
+                if let Err(err) = result {
+                    log::error!("Failed to process transfer: {:?}", err);
+                }
             }
             Err(e) => {
                 info!("Failed to decode base64 data: {:?}", e);
