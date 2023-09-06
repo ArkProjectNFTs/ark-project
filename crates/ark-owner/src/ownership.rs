45b7df31a9a3b546031b6bf87d74a0f91d2ad116
kwiss <mr.dumont.christophe@gmail.com>
2023-09-06 16:55:36 +0200

    fix(ownership): remove old crate

--- crates/ark-owner/src/ownership.rs
+++
@@ -1,4 +0,0 @@
-// TODO
-// if mint add token to owner in db for collection_address && token_id
-// if transfer remove token from owner in db for collection_address && token_id
-// if transfer add token to owner in db for collection_address && token_id
