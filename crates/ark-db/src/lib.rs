914f30a3650601557967056fb7af2c1f55b0c832
Rémi Roycourt <remiroycourt@gmail.com>
2023-09-07 06:33:00 +0200

    feat(core): update indexer status

--- crates/ark-db/src/lib.rs
+++ crates/ark-db/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod block;
 pub mod collection;
 pub mod contract;
+pub mod indexer;
 pub mod owners;
 pub mod token;
 pub mod token_event;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 mod constants;
 mod core;
 mod utils;
-use std::env;
-
 use crate::core::block::process_blocks_continuously;
 use anyhow::Result;
+use ark_db::indexer::create::create_indexer;
 use ark_starknet::{client2::StarknetClient, collection_manager::CollectionManager};
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_dynamodb::{types::AttributeValue, Client as DynamoClient};
+use aws_sdk_dynamodb::Client as DynamoClient;
 use aws_sdk_kinesis::Client as KinesisClient;
-use chrono::Utc;
 use dotenv::dotenv;
 use reqwest::{Client as ReqwestClient, Url};
 use simple_logger::SimpleLogger;
 use starknet::providers::{jsonrpc::HttpTransport, JsonRpcClient};
+use std::env;
+
+fn extract_ecs_task_id(text: &str) -> Option<&str> {
+    let pattern = regex::Regex::new(r"/v3/([a-f0-9]{32})-").unwrap();
+    pattern
+        .captures(text)
+        .and_then(|cap| cap.get(1).map(|m| m.as_str()))
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -34,20 +40,12 @@ async fn main() -> Result<()> {
     let dynamo_client = DynamoClient::new(&config);
     let reqwest_client = ReqwestClient::new();
 
-    let indexer_table_name =
-        env::var("ARK_INDEXER_TABLE_NAME").expect("ARK_INDEXER_TABLE_NAME must be set");
+    let container_metadata_uri = env::var("ECS_CONTAINER_METADATA_URI").unwrap_or("".to_string());
+    let task_id = extract_ecs_task_id(container_metadata_uri.as_str());
 
-    let indexer_version = env::var("ARK_INDEXER_VERSION").unwrap_or(String::from("undefined"));
-    let now = Utc::now();
-    let unix_timestamp = now.timestamp();
-
-    dynamo_client
-        .put_item()
-        .table_name(indexer_table_name)
-        .item("PK", AttributeValue::S(String::from("ECS_")))
-        .item("SK", AttributeValue::S(unix_timestamp.to_string()))
-        .item("status", AttributeValue::S(String::from("running")))
-        .item("version", AttributeValue::S(String::from(indexer_version)));
+    if task_id.is_some() {
+        create_indexer(&dynamo_client, task_id.unwrap()).await?;
+    }
 
     process_blocks_continuously(
         &collection_manager,
