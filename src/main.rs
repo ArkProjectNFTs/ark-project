521ab1349526cc9abaceae51bd43f5b9ce7cfdae
Rémi Roycourt <remiroycourt@gmail.com>
2023-09-07 06:08:46 +0200

    feat(core): insert into indexer table

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use crate::core::block::process_blocks_continuously;
 use anyhow::Result;
 use ark_starknet::{client2::StarknetClient, collection_manager::CollectionManager};
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_dynamodb::Client as DynamoClient;
+use aws_sdk_dynamodb::{types::AttributeValue, Client as DynamoClient};
 use aws_sdk_kinesis::Client as KinesisClient;
+use chrono::Utc;
 use dotenv::dotenv;
 use reqwest::{Client as ReqwestClient, Url};
 use simple_logger::SimpleLogger;
@@ -32,6 +33,21 @@ async fn main() -> Result<()> {
     let kinesis_client = KinesisClient::new(&config);
     let dynamo_client = DynamoClient::new(&config);
     let reqwest_client = ReqwestClient::new();
+
+    let indexer_table_name =
+        env::var("ARK_INDEXER_TABLE_NAME").expect("ARK_INDEXER_TABLE_NAME must be set");
+
+    let indexer_version = env::var("ARK_INDEXER_VERSION").unwrap_or(String::from("undefined"));
+    let now = Utc::now();
+    let unix_timestamp = now.timestamp();
+
+    dynamo_client
+        .put_item()
+        .table_name(indexer_table_name)
+        .item("PK", AttributeValue::S(String::from("ECS_")))
+        .item("SK", AttributeValue::S(unix_timestamp.to_string()))
+        .item("status", AttributeValue::S(String::from("running")))
+        .item("version", AttributeValue::S(String::from(indexer_version)));
 
     process_blocks_continuously(
         &collection_manager,
