6dd7807409b2349f7b4be378ff6bfe931de6d91d
glihm <dev@glihm.net>
2023-09-13 08:53:58 -0600

    move logic to crate instead of main

---
+++ crates/ark-core/src/managers/collection_manager.rs
@@ -0,0 +1,177 @@
+use anyhow::{anyhow, Result};
+use ark_starknet::client2::StarknetClient;
+use ark_storage::storage_manager::StorageManager;
+
+use serde::{Deserialize, Serialize};
+use starknet::core::types::{BlockId, BlockTag, FieldElement};
+use starknet::core::utils::{get_selector_from_name, parse_cairo_short_string};
+
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum ContractType {
+    Other,
+    ERC721,
+    ERC1155,
+}
+
+impl ToString for ContractType {
+    fn to_string(&self) -> String {
+        match self {
+            ContractType::Other => "other".to_string(),
+            ContractType::ERC721 => "erc721".to_string(),
+            ContractType::ERC1155 => "erc1155".to_string(),
+        }
+    }
+}
+
+// TODO: this struct must come from Storage crate.
+#[derive(Debug, Clone)]
+pub struct ContractInfo {
+    pub name: String,
+    pub symbol: String,
+    pub r#type: ContractType,
+}
+
+pub struct CollectionManager<'a, T: StorageManager> {
+    storage: &'a T,
+    client: &'a StarknetClient,
+    /// A cache with contract address mapped to it's type.
+    cache: HashMap<FieldElement, ContractInfo>,
+}
+
+impl<'a, T: StorageManager> CollectionManager<'a, T> {
+    /// Initializes a new instance.
+    pub fn new(storage: &'a T, client: &'a StarknetClient) -> Self {
+        Self {
+            storage,
+            client,
+            cache: HashMap::new(),
+        }
+    }
+
+    /// Gets the contract info from local cache, or fetch is from the DB.
+    fn get_cached_or_fetch_info(&mut self, address: FieldElement) -> Result<ContractInfo> {
+        match self.cache.get(&address) {
+            Some(info) => Ok(info.clone()),
+            None => {
+                log::trace!("Cache miss for contract {address}");
+                // TODO: self.storage.get_contract_info();
+                // If no info available -> return error.
+                // For now, return error to simulate it's not available.
+                Err(anyhow!("Info not found in storage for contract {address}"))
+            }
+        }
+    }
+
+    /// Identifies a contract from it's address only.
+    pub async fn identify_contract(&mut self, address: FieldElement) -> Result<ContractInfo> {
+        // The cache is more efficient that formatting to check the BLACKLIST.
+        match self.get_cached_or_fetch_info(address) {
+            Ok(info) => Ok(info),
+            Err(_) => {
+                // Can't find info, try to identify with calls.
+                let contract_type = self.get_contract_type(address).await?;
+
+                log::info!(
+                    "New contract identified [{:#064x}] : {}",
+                    address,
+                    contract_type.to_string()
+                );
+
+                let info = ContractInfo {
+                    name: String::new(),
+                    symbol: String::new(),
+                    r#type: contract_type,
+                };
+
+                self.cache.insert(address, info.clone());
+                // TODO: self.storage.register_contract_info(...);
+
+                Ok(info)
+            }
+        }
+    }
+
+    pub async fn get_contract_type(&self, contract_address: FieldElement) -> Result<ContractType> {
+        let block = BlockId::Tag(BlockTag::Latest);
+        let token_uri_cairo_0 = self
+            .get_contract_property_string(
+                contract_address,
+                "tokenURI",
+                vec![FieldElement::ONE, FieldElement::ZERO],
+                block,
+            )
+            .await
+            .unwrap_or("undefined".to_string());
+
+        let token_uri = self
+            .get_contract_property_string(
+                contract_address,
+                "token_uri",
+                vec![FieldElement::ONE, FieldElement::ZERO],
+                block,
+            )
+            .await
+            .unwrap_or("undefined".to_string());
+
+        let uri_result = self
+            .get_contract_property_string(contract_address, "uri", vec![], block)
+            .await
+            .unwrap_or("undefined".to_string());
+
+        if (token_uri_cairo_0 != "undefined" && !token_uri_cairo_0.is_empty())
+            || (token_uri != "undefined" && !token_uri.is_empty())
+        {
+            Ok(ContractType::ERC721)
+        } else if uri_result != "undefined" {
+            Ok(ContractType::ERC1155)
+        } else {
+            Ok(ContractType::Other)
+        }
+    }
+
+    pub async fn get_contract_property_string(
+        &self,
+        contract_address: FieldElement,
+        selector_name: &str,
+        calldata: Vec<FieldElement>,
+        block: BlockId,
+    ) -> Result<String> {
+        let response = self
+            .client
+            .call_contract(
+                contract_address,
+                get_selector_from_name(selector_name)?,
+                calldata,
+                block,
+            )
+            .await?;
+
+        decode_string_array(&response)
+    }
+}
+
+pub fn decode_string_array(string_array: &Vec<FieldElement>) -> Result<String> {
+    match string_array.len() {
+        0 => Ok("".to_string()),
+        1 => Ok(parse_cairo_short_string(&string_array[0])?),
+        2 => Ok(format!(
+            "{}{}",
+            parse_cairo_short_string(&string_array[0])?,
+            parse_cairo_short_string(&string_array[1])?,
+        )),
+        _ => {
+            // The first element is the length of the string,
+            // we can skip it as it's implicitely given by the vector itself.
+            let mut result = String::new();
+
+            for s in &string_array[1..] {
+                result.push_str(&parse_cairo_short_string(s)?);
+            }
+
+            Ok(result)
+        }
+    }
+}
---
+++ crates/ark-core/src/managers/token_manager.rs
@@ -0,0 +1,108 @@
+use anyhow::{anyhow, Result};
+use ark_starknet::client2::StarknetClient;
+use ark_storage::storage_manager::StorageManager;
+use ark_storage::types::{EventType, TokenEvent, TokenFromEvent};
+use starknet::core::types::*;
+use starknet::macros::selector;
+
+#[derive(Debug)]
+pub struct TokenManager<'a, T: StorageManager> {
+    storage: &'a T,
+    client: &'a StarknetClient,
+    // TODO: Same as event manager, we should use the stack instead.
+    // check with @kwiss.
+    token: TokenFromEvent,
+}
+
+impl<'a, T: StorageManager> TokenManager<'a, T> {
+    /// Initializes a new instance.
+    pub fn new(storage: &'a T, client: &'a StarknetClient) -> Self {
+        Self {
+            storage,
+            client,
+            token: TokenFromEvent::default(),
+        }
+    }
+
+    /// Formats a token registry from the token event data.
+    pub async fn format_token(&mut self, event: &TokenEvent) -> Result<()> {
+        self.reset_token();
+
+        self.token.address = event.contract_address.clone();
+        self.token.padded_token_id = event.padded_token_id.clone();
+        self.token.from_address = event.from_address.clone();
+        self.token.to_address = event.to_address.clone();
+        self.token.timestamp = event.timestamp.clone();
+        self.token.mint_transaction_hash = if event.event_type == EventType::Mint {
+            Some(event.transaction_hash.clone())
+        } else {
+            None
+        };
+        self.token.block_number_minted = if event.event_type == EventType::Mint {
+            Some(event.block_number)
+        } else {
+            None
+        };
+
+        // TODO: @kwiss, do we want a default value in case we can't get the token owner?
+        // or do we want to return an error and abort before saving in the storage?
+        let token_owner = self
+            .get_token_owner(
+                FieldElement::from_hex_be(&event.contract_address)
+                    .expect("Contract address bad format"),
+                FieldElement::from(event.token_id.low),
+                FieldElement::from(event.token_id.high),
+            )
+            .await?[0];
+
+        self.token.owner = format!("{:#064x}", token_owner);
+
+        log::trace!(
+            "Registering token: {} {}",
+            event.token_id.format().token_id,
+            event.contract_address
+        );
+
+        // TODO: self.storage.register_token(self.token.clone()).await?;
+
+        Ok(())
+    }
+
+    ///
+    pub fn reset_token(&mut self) {
+        self.token = TokenFromEvent::default();
+    }
+
+    /// Retrieves the token owner for the last block.
+    pub async fn get_token_owner(
+        &self,
+        contract_address: FieldElement,
+        token_id_low: FieldElement,
+        token_id_high: FieldElement,
+    ) -> Result<Vec<FieldElement>> {
+        let block = BlockId::Tag(BlockTag::Latest);
+
+        match self
+            .client
+            .call_contract(
+                contract_address,
+                selector!("owner_of"),
+                vec![token_id_low, token_id_high],
+                block,
+            )
+            .await
+        {
+            Ok(res) => Ok(res),
+            Err(_) => self
+                .client
+                .call_contract(
+                    contract_address,
+                    selector!("ownerOf"),
+                    vec![token_id_low, token_id_high],
+                    block,
+                )
+                .await
+                .map_err(|_| anyhow!("Failed to get token owner from chain")),
+        }
+    }
+}
