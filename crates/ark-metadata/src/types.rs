931843ce755f5dcd5db85a35e45387d87270bfa6
Rémi Roycourt <11146088+remiroyc@users.noreply.github.com>
2023-10-25 22:17:08 +0200

    refactor(ark-metadata): rename structs & add metadata properties (#161)

--- crates/ark-metadata/src/types.rs
+++ crates/ark-metadata/src/types.rs
@@ -1,6 +1,6 @@
 use serde_derive::{Deserialize, Serialize};
 use serde_json::Number;
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
 #[derive(Debug, PartialEq)]
 pub enum MetadataType {
@@ -49,26 +49,22 @@ impl fmt::Display for DisplayType {
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(untagged)]
-pub enum MetadataAttributeValue {
+pub enum MetadataTraitValue {
     String(String),
     Number(Number),
-    Bool(bool),
-    StringVec(Vec<String>),
-    NumberVec(Vec<Number>),
-    BoolVec(Vec<bool>),
 }
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct MetadataAttribute {
     pub display_type: Option<DisplayType>,
     pub trait_type: Option<String>,
-    pub value: MetadataAttributeValue,
+    pub value: MetadataTraitValue,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
 pub struct TokenMetadata {
-    pub raw: String,
     pub normalized: NormalizedMetadata,
+    pub raw: String,
 }
 
 #[derive(Debug, Default, Deserialize, Serialize)]
@@ -79,7 +75,15 @@ pub struct NormalizedMetadata {
     pub description: Option<String>,
     pub name: Option<String>,
     pub attributes: Option<Vec<MetadataAttribute>>,
+    pub properties: Option<HashMap<String, MetadataProperty>>,
     pub background_color: Option<String>,
     pub animation_url: Option<String>,
     pub youtube_url: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct MetadataProperty {
+    #[serde(rename = "type")]
+    property_type: String,
+    description: String,
 }
