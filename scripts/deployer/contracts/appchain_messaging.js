02e4dabe1da396fb3a397f409848117563f7068c
kwiss <mr.dumont.christophe@gmail.com>
2023-12-10 14:23:42 +0100

    feat(deployer): update deployers

---
+++ scripts/deployer/contracts/appchain_messaging.js
@@ -0,0 +1,39 @@
+import * as sn from "starknet";
+
+import * as common from "./common.js";
+
+/**
+ * Declare and deploys appchain_messaging contract.
+ * Returns the contract object.
+ */
+export async function declareDeploy(
+  artifacts_path,
+  account,
+  provider,
+  constructorData
+) {
+  const artifacts = common.load_artifacts(
+    artifacts_path,
+    "ark_starknet_appchain_messaging"
+  );
+
+  const contractCallData = new sn.CallData(artifacts.sierra.abi);
+  const contractConstructor = contractCallData.compile("constructor", {
+    owner: constructorData.owner,
+    appchain_account: constructorData.appchain_account
+  });
+
+  // TODO: don't know how to speed up the polling of receipts... it's TOO SLOW!
+  const deployR = await account.declareAndDeploy({
+    contract: artifacts.sierra,
+    casm: artifacts.casm,
+    constructorCalldata: contractConstructor,
+    salt: 0x12345678
+  });
+
+  return new sn.Contract(
+    artifacts.sierra.abi,
+    deployR.deploy.contract_address,
+    provider
+  );
+}
--- scripts/deployer/deploy-sn.js
+++ scripts/deployer/deploy-sn.js
@@ -1,31 +1,33 @@
 // IMPORTANT! Don't upgrade starknetjs, as katana does not support v0.5.x RPC version
 // for the moment...
 //
-import fs from "fs";
 
+import * as dotenv from "dotenv";
 import * as sn from "starknet";
 
 import * as appmsg from "./contracts/appchain_messaging.js";
 import * as executor from "./contracts/executor.js";
 import * as orderbook from "./contracts/orderbook.js";
 
+dotenv.config();
 const STARKGATE =
   "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
 // Deployer script for ark-project contracts.
 // Before running the script, both node must be running.
-const katana = new sn.RpcProvider({ nodeUrl: "http://127.0.0.1:5050" });
-const solis = new sn.RpcProvider({
-  nodeUrl: "https://staging.solis.arkproject.dev"
+const snRpc = new sn.RpcProvider({ nodeUrl: process.env.SN_RPC });
+const solisRpc = new sn.RpcProvider({
+  nodeUrl: process.env.SOLIS_RPC
 });
 
 // Katana-0 account is used in both Katana and Solis as they use the same
 // seed to initialize accounts.
-const privkey0 = "0x1800000000300000180000000000030000000000003006001800006600";
-const account0 =
-  "0x517ececd29116499f4a1b64b094da79ba08dfd54a3edaa316134c41f8160973";
-const katana_account0 = new sn.Account(katana, account0, privkey0);
-const solis_account0 = new sn.Account(solis, account0, privkey0);
+const snPrivKey = process.env.SN_PRIV_KEY;
+const snAccount = process.env.SN_ACCOUNT;
+const solisPrivKey = process.env.SOLIS_PRIV_KEY;
+const solisAccount = process.env.SOLIS_ACCOUNT;
+const sn_account = new sn.Account(snRpc, snAccount, snPrivKey);
+const solis_account = new sn.Account(solisRpc, solisAccount, solisPrivKey);
 
 const sn_artifacts_path = "../../crates/ark-contracts/starknet/target/dev/";
 const arkchain_artifacts_path =
@@ -33,23 +35,24 @@ const arkchain_artifacts_path =
 
 let appmsg_contract = await appmsg.declareDeploy(
   sn_artifacts_path,
-  katana_account0,
-  katana,
+  sn_account,
+  snRpc,
   {
-    owner: account0,
-    appchain_account: account0
+    owner: snAccount,
+    appchain_account: solisAccount
   }
 );
+
 console.log("appmessaging (sn)", appmsg_contract.address);
 
 let executor_contract = await executor.declareDeploy(
   sn_artifacts_path,
-  katana_account0,
-  katana,
+  sn_account,
+  snRpc,
   {
-    admin_address: account0,
+    admin_address: snAccount,
     eth_contract_address: STARKGATE,
-    arkchain_orderbook_address: 0x1234,
+    arkchain_orderbook_address: process.env.ORDERBOOK_ADDRESS,
     messaging_address: appmsg_contract.address
   }
 );
@@ -57,18 +60,10 @@ console.log("executor (sn)", executor_contract.address);
 
 let orderbook_contract = await orderbook.declareDeploy(
   arkchain_artifacts_path,
-  solis_account0,
-  solis,
+  solis_account,
+  solisRpc,
   {
-    admin: account0
+    admin: solis_account
   }
 );
 console.log("orderbook (ark)", orderbook_contract.address);
-
-// TODO: Solis requires the address of the orderbook + the executor address,
-// So if the code changes for any of those contracts, the address must be pre-computed
-// using starkli to start Solis accordingly.
-
-// TODO: Create order (SDK).
-// TODO: fulfill order (SDK).
-// TODO: the messaging should then work.
