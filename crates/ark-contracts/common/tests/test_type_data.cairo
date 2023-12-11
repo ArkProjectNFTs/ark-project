2268da32107912258fe329879649dca0ad954fa4
kwiss <mr.dumont.christophe@gmail.com>
2023-12-11 17:38:48 +0100

    feat(signature): add signature test

--- crates/ark-contracts/common/tests/test_type_data.cairo
+++ crates/ark-contracts/common/tests/test_type_data.cairo
@@ -13,21 +13,24 @@ use snforge_std::signature::{
     StarkCurveKeyPair, StarkCurveKeyPairTrait, Signer as SNSigner, Verifier
 };
 
-// fn sign_mock(message_hash: felt252) -> Signer {
-//     let private_key: felt252 = 0x1234567890987654321;
-//     let mut key_pair = StarkCurveKeyPairTrait::from_private_key(private_key);
-//     let (r, s) = key_pair.sign(message_hash).unwrap();
-//     Signer::WEIERSTRESS_STARKNET(
-//         SignInfo { user_pubkey: key_pair.public_key, user_sig_r: r, user_sig_s: s, }
-//     )
-// }
+fn sign_mock(message_hash: felt252) -> Signer {
+    let private_key: felt252 = 0x1234567890987654321;
+    let mut key_pair = StarkCurveKeyPairTrait::from_private_key(private_key);
+    let (r, s) = key_pair.sign(message_hash).unwrap();
+    r.print();
+    s.print();
+    key_pair.public_key.print();
+    Signer::WEIERSTRESS_STARKNET(
+        SignInfo { user_pubkey: key_pair.public_key, user_sig_r: r, user_sig_s: s, }
+    )
+}
 
 fn DOMAIN() -> Domain {
     Domain { name: 'Ark', version: '1.1', }
 }
 
 fn SIGNER() -> starknet::ContractAddress {
-  starknet::contract_address_const::<'signer'>()
+  0x2284a6517b487be8114013f277f9e2010ac001a24a93e3c48cdf5f8f345a81b.try_into().unwrap()
 }
 
 fn ORDER() -> Order {
@@ -35,14 +38,13 @@ fn ORDER() -> Order {
 }
 
 fn ORDER_HASH() -> felt252 {
-  0x51cf3229352fd8c0265d8897fedf67f86d4b5ec580513f922b963ce47ae1a16
+  0x32c0fac84cfd23ed5a608215640aea894432b9f57c2f0eeeef8890b69c9857a
 }
 
 #[test]
 fn ark_test_type_data() {
     let order = ORDER();
     let order_hash = order.compute_hash_from(from: SIGNER(), domain: DOMAIN());
-    order_hash.print();
+    let signer = sign_mock(order_hash);
     assert(order_hash == ORDER_HASH(), 'Invalid order hash');
-    // let signer = sign_mock(order_hash);
 }
--- packages/core/examples/signTypeData.ts
+++ packages/core/examples/signTypeData.ts
@@ -2,23 +2,23 @@ import {
   RpcProvider,
   selector,
   shortString,
-  TypedData,
+  Signature,
   typedData
 } from "starknet";
 
-import { createAccount } from "../src/actions/account/account";
+import { createAccount } from "../src/actions/account";
 
 const provider = new RpcProvider({
   nodeUrl: "http://0.0.0.0:5050"
 });
 
 (async (provider: RpcProvider) => {
-  // const { account, publicKey } = await createAccount(provider);
+  const { account } = await createAccount(provider);
+  console.log(account.address);
+  // const orderTypeHash = selector.starknetKeccak("Order(orderHash:felt252)");
+  // console.log("orderTypeHash: " + orderTypeHash);
 
-  const orderTypeHash = selector.starknetKeccak("Order(orderHash:felt252)");
-  console.log("orderTypeHash: " + orderTypeHash);
-
-  const voucherData = {
+  const TypedOrderData = {
     message: {
       orderHash: 1
     },
@@ -38,9 +38,14 @@ const provider = new RpcProvider({
     primaryType: "Order"
   };
 
-  const data = voucherData;
-  const addr = shortString.encodeShortString("signer");
-  console.log(typedData.getMessageHash(data, addr));
+  const data = TypedOrderData;
+
+  console.log(
+    typedData.getMessageHash(
+      data,
+      "0x2284a6517b487be8114013f277f9e2010ac001a24a93e3c48cdf5f8f345a81b"
+    )
+  );
 
   // console.log("domain hash: " + domainHash);
   // console.log("-------------------");
@@ -62,13 +67,13 @@ const provider = new RpcProvider({
   // console.log("orderTypeHash: " + orderTypeHash);
   // console.log("domainTypeHash: " + domainTypeHash);
 
-  // let signer: Signature = await account.signMessage(typedDataValidate);
-  // if (Array.isArray(signer)) {
-  //   for (const item of signer) {
-  //     console.log(item);
-  //   }
-  // } else {
-  //   console.log("signer r: " + signer.r);
-  //   console.log("signer s: " + signer.s);
-  // }
+  let signer: Signature = await account.signMessage(TypedOrderData);
+  if (Array.isArray(signer)) {
+    for (const item of signer) {
+      console.log(item);
+    }
+  } else {
+    console.log("signer r: " + signer.r);
+    console.log("signer s: " + signer.s);
+  }
 })(provider);
