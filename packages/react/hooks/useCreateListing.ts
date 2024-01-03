6c2e6d27e6c7ac53f500d4fc421b995147e71fff
Rémi Roycourt <remiroycourt@gmail.com>
2024-01-03 11:22:23 +0100

    feat(core): approve

--- packages/react/hooks/useCreateListing.ts
+++ packages/react/hooks/useCreateListing.ts
@@ -1,11 +1,18 @@
 import { useState } from "react";
 
-import { Account, AccountInterface } from "starknet";
+import {
+  Account,
+  AccountInterface,
+  BigNumberish,
+  cairo,
+  CallData
+} from "starknet";
 
 import {
   createListing as createListingCore,
   ListingV1
 } from "@ark-project/core";
+import { getContractAddresses } from "@ark-project/core/src/constants";
 
 import { useRpc } from "../components/ArkProvider/RpcContext";
 import { Status } from "../types/hooks";
@@ -13,10 +20,28 @@ import { useOwner } from "./useOwner";
 
 export default function useCreateListing() {
   const { rpcProvider, network } = useRpc();
-
   const [status, setStatus] = useState<Status>("idle");
   const [response, setResponse] = useState<bigint | undefined>(undefined);
   const owner = useOwner();
+  const { STARKNET_EXECUTOR_ADDRESS } = getContractAddresses(network);
+
+  async function authorizeStarknetERC721Transfer(
+    tokenAddress: string,
+    starknetAccount: AccountInterface,
+    tokenId: BigNumberish
+  ) {
+    await starknetAccount.execute([
+      {
+        contractAddress: tokenAddress,
+        entrypoint: "approve",
+        calldata: CallData.compile({
+          to: STARKNET_EXECUTOR_ADDRESS,
+          token_id: cairo.uint256(tokenId)
+        })
+      }
+    ]);
+  }
+
   async function createListing(
     starknetAccount: AccountInterface,
     order: ListingV1
@@ -24,6 +49,7 @@ export default function useCreateListing() {
     const burner_address = localStorage.getItem("burner_address");
     const burner_private_key = localStorage.getItem("burner_private_key");
     const burner_public_key = localStorage.getItem("burner_public_key");
+
     if (
       burner_address === null ||
       burner_private_key === null ||
@@ -34,6 +60,13 @@ export default function useCreateListing() {
 
     try {
       setStatus("loading");
+
+      await authorizeStarknetERC721Transfer(
+        order.tokenAddress.toString(),
+        starknetAccount,
+        order.tokenId
+      );
+
       const orderHash = await createListingCore(
         network,
         rpcProvider,
