6e00d1eef81d09c26780ef06967b36fd73daecdc
kwiss <kwiss@users.noreply.github.com>
2024-02-26 09:58:37 +0000

    chore: format

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -1,10 +1,12 @@
 // This file is auto-generated. Do not edit directly.
 
 export const GOERLI_CONTRACTS = {
-  "nftContract": "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
-  "messaging": "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
-  "executor": "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
-  "orderbook": "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
+  nftContract:
+    "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
+  messaging:
+    "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
+  executor: "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
+  orderbook: "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
 };
 export const SEPOLIA_CONTRACTS = {};
 export const MAINNET_CONTRACTS = {};
