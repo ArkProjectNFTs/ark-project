420d012b8375469e94ca657db42c5b51c4da300e
bexan <y.bensacq@gmail.com>
2024-02-28 16:43:51 +0100

    Hooker & js examples

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -10,11 +10,4 @@ export const GOERLI_CONTRACTS = {
 };
 export const SEPOLIA_CONTRACTS = {};
 export const MAINNET_CONTRACTS = {};
-export const DEV_CONTRACTS = {
-  nftContract:
-    "0x6f63d7d261c2d2c5409ba80ab0b235303fdcd01daeef61ca5ae7238285d7b78",
-  eth: "0x7b8fc5c78bf249bb4173ef45267da1a710c65d76bb9ff03deb6b8fc21f35ba4",
-  messaging: "0x38e142d3d69cfe6757fd9d11704652f6bd8302dd23f483555e9b5698bf69d5",
-  executor: "0x1f303987f8a61ee25d5a153791fd778f0c478947bb32613d15fc8194e0360a6",
-  orderbook: "0x3d7a3a02741c905fdb3cccf14e376a7d64c8cb071874f47f0b01d73015eae6b"
-};
+export const DEV_CONTRACTS = {};
