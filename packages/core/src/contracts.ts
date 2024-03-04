6021bfb5481cd639837e34a9aa268ef52139143a
kwiss <kwiss@users.noreply.github.com>
2024-03-04 09:30:14 +0000

    chore: format

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -1,17 +1,21 @@
 // This file is auto-generated. Do not edit directly.
 
 export const GOERLI_CONTRACTS = {
-  "nftContract": "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
-  "messaging": "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
-  "executor": "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
-  "orderbook": "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
+  nftContract:
+    "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
+  messaging:
+    "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
+  executor: "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
+  orderbook: "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
 };
 export const SEPOLIA_CONTRACTS = {};
 export const MAINNET_CONTRACTS = {};
 export const DEV_CONTRACTS = {
-  "messaging": "0x1fed294721e45a912cd64ec6aba8b9e902b6f54fd516547ca740805607d47cd",
-  "executor": "0x4a69823e775f40d8837c62cf9e239c3da4ab65888dd6d8f19e6f9b11771e460",
-  "nftContract": "0x664b395332553a06c12f6164f11544b92b1ec308fa6c360f01f9cde1cc32ac2",
-  "eth": "0x4007c4afac7b79c2119c1d417e3106294cb28d92bdfba26474e8e250b1e91c9",
-  "orderbook": "0x1b59160f5593a11ea887b063d541a42f758d82e0b7295763e3104e09057ab5"
+  messaging:
+    "0x1fed294721e45a912cd64ec6aba8b9e902b6f54fd516547ca740805607d47cd",
+  executor: "0x4a69823e775f40d8837c62cf9e239c3da4ab65888dd6d8f19e6f9b11771e460",
+  nftContract:
+    "0x664b395332553a06c12f6164f11544b92b1ec308fa6c360f01f9cde1cc32ac2",
+  eth: "0x4007c4afac7b79c2119c1d417e3106294cb28d92bdfba26474e8e250b1e91c9",
+  orderbook: "0x1b59160f5593a11ea887b063d541a42f758d82e0b7295763e3104e09057ab5"
 };
