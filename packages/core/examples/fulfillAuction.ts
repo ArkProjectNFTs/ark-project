9829faf1cf41be50f3e663b6f7b03ce0d782de0b
paul launay <launay.p@gmail.com>
2024-04-11 18:54:34 +0200

    feat(sdk): add auction examples

---
+++ packages/core/examples/fulfillAuction.ts
@@ -0,0 +1,114 @@
+import { shortString } from "starknet";
+
+import "dotenv/config";
+
+import { stark } from "starknet";
+
+import {
+  approveERC721,
+  AuctionV1,
+  createAuction,
+  createOffer,
+  fetchOrCreateAccount,
+  fulfillAuction,
+  getOrderStatus,
+  OfferV1
+} from "../src";
+import { createBroker } from "../src/actions/broker/createBroker";
+import { FulfillAuctionInfo } from "../src/types";
+import { config } from "./config";
+import { STARKNET_NFT_ADDRESS } from "./constants";
+import { getCurrentTokenId } from "./utils/getCurrentTokenId";
+import { mintERC721 } from "./utils/mintERC721";
+import { whitelistBroker } from "./utils/whitelistBroker";
+
+(async () => {
+  // Create test accounts
+  const adminAccount = await fetchOrCreateAccount(
+    config.arkProvider,
+    process.env.SOLIS_ADMIN_ADDRESS,
+    process.env.SOLIS_ADMIN_PRIVATE_KEY
+  );
+
+  const sellerAccount = await fetchOrCreateAccount(
+    config.starknetProvider,
+    process.env.STARKNET_ACCOUNT1_ADDRESS,
+    process.env.STARKNET_ACCOUNT1_PRIVATE_KEY
+  );
+
+  const buyerAccount = await fetchOrCreateAccount(
+    config.starknetProvider,
+    process.env.STARKNET_ACCOUNT2_ADDRESS,
+    process.env.STARKNET_ACCOUNT2_PRIVATE_KEY
+  );
+
+  // Create and whitelist broker
+  const brokerId = stark.randomAddress();
+  await createBroker(config, { brokerID: brokerId });
+  await whitelistBroker(config, adminAccount, brokerId);
+
+  // Mint and approve seller NFT
+  const tokenId = await getCurrentTokenId(config, STARKNET_NFT_ADDRESS);
+
+  await mintERC721(config.starknetProvider, sellerAccount);
+  await approveERC721(config, {
+    contractAddress: STARKNET_NFT_ADDRESS,
+    starknetAccount: sellerAccount,
+    tokenId
+  });
+
+  // Create auction
+  const order: AuctionV1 = {
+    brokerId,
+    tokenAddress: STARKNET_NFT_ADDRESS,
+    tokenId: tokenId,
+    startAmount: 1,
+    endAmount: 10
+  };
+
+  const orderHash = await createAuction(config, {
+    starknetAccount: sellerAccount,
+    arkAccount: adminAccount,
+    order
+  });
+
+  // Create offer
+  const offer: OfferV1 = {
+    brokerId,
+    tokenAddress: STARKNET_NFT_ADDRESS,
+    tokenId: tokenId,
+    startAmount: 1
+  };
+
+  const offerOrderHash = await createOffer(config, {
+    starknetAccount: buyerAccount,
+    arkAccount: adminAccount,
+    offer
+  });
+
+  // Fulfill auction
+  const fulfillAuctionInfo: FulfillAuctionInfo = {
+    orderHash,
+    relatedOrderHash: offerOrderHash,
+    tokenAddress: order.tokenAddress,
+    tokenId,
+    brokerId
+  };
+
+  await fulfillAuction(config, {
+    starknetAccount: sellerAccount,
+    arkAccount: adminAccount,
+    fulfillAuctionInfo
+  });
+
+  await new Promise((resolve) => setTimeout(resolve, 10_000));
+
+  const { orderStatus: orderStatus } = await getOrderStatus(config, {
+    orderHash
+  });
+
+  console.log(
+    "Auction order status: ",
+    shortString.decodeShortString(orderStatus)
+  );
+})();
