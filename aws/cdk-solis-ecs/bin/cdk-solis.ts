f746d93b0fc37256dd64a19ce4e00035c7738c28
kwiss <mr.dumont.christophe@gmail.com>
2024-05-23 19:14:43 +0200

    feat(solis): add lambda to create bd folder

--- aws/cdk-solis-ecs/bin/cdk-solis.ts
+++ aws/cdk-solis-ecs/bin/cdk-solis.ts
@@ -1,6 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 
-// import { ArkSolisLambdaStack } from "../lib/cdk-solis-db";
+import { ArkSolisLambdaStack } from "../lib/cdk-solis-db";
 // import { ArkSolisEcsStack } from "../lib/cdk-solis-stack";
 import { ArkSolisEfsStack } from "../lib/cdk-stack-efs";
 
@@ -13,12 +13,12 @@ new ArkSolisEfsStack(app, "ark-solis-efs-stack", {
   }
 });
 
-// new ArkSolisLambdaStack(app, "ark-solis-lambda-stack", {
-//   env: {
-//     account: process.env.CDK_DEFAULT_ACCOUNT,
-//     region: process.env.CDK_DEFAULT_REGION
-//   }
-// });
+new ArkSolisLambdaStack(app, "ark-solis-lambda-stack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
 
 // new ArkSolisEcsStack(app, "ark-solis-production-stack", {
 //   vpcId: "vpc-0d11f7ec183208e08",
