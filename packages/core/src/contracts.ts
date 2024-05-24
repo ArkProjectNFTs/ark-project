05492f25b9cd54fb534b30bff33386a091118e30
kwiss <mr.dumont.christophe@gmail.com>
2024-05-24 15:45:28 +0200

    fix(contracts): fix merge conflict

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -1,32 +1,28 @@
 // This file is auto-generated. Do not edit directly.
 
 export const GOERLI_CONTRACTS = {
-  "nftContract": "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
-  "messaging": "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
-  "executor": "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
-  "orderbook": "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
+  nftContract:
+    "0x22411b480425fe6e627fdf4d1b6ac7f8567314ada5617a0a6d8ef3e74b69436",
+  messaging:
+    "0x2c3d3e0c37d29364a13ba8cff046e7bc5624655a72526961876a1c8bb3f63c8",
+  executor: "0x73148536f8ea9546e92761d11515548cc433df46883d5ee98871a6f63a0bbbc",
+  orderbook: "0x66f1e6acf9bdbd23837b2eea271430298b355c506978edb132737e7fcb6b310"
 };
 export const SEPOLIA_CONTRACTS = {};
 export const MAINNET_CONTRACTS = {
-  "nftContract": "0x32d99485b22f2e58c8a0206d3b3bb259997ff0db70cffd25585d7dd9a5b0546",
-  "messaging": "0x57d45cc46de463f7ae63b74ce9b6b6b496a1178b02e7ad04d7c307caa698b7b",
-  "executor": "0x7b42945bc47001db92fe1b9739d753925263f2f1036c2ae1f87536c916ee6a",
-  "orderbook": "0x785873b81c8a3f076270868418c783e8faa5f2b86b87a1754947c650868feb8"
+  nftContract:
+    "0x32d99485b22f2e58c8a0206d3b3bb259997ff0db70cffd25585d7dd9a5b0546",
+  messaging:
+    "0x57d45cc46de463f7ae63b74ce9b6b6b496a1178b02e7ad04d7c307caa698b7b",
+  executor: "0x7b42945bc47001db92fe1b9739d753925263f2f1036c2ae1f87536c916ee6a",
+  orderbook: "0x785873b81c8a3f076270868418c783e8faa5f2b86b87a1754947c650868feb8"
 };
 export const DEV_CONTRACTS = {
-<<<<<<< feature/dev-470-hooker
-  "messaging": "0x6e40ed6d24f08a5ec9dc1017c5c6542a002c2d9c5dbed0e95ce930e9eb40d58",
-  "executor": "0x5a9a84950d5413d00094e9e8eb674d8aae962fc9f607be6d2dd35e516d6b4e7",
-  "nftContract": "0x87db65c66b914a83bbea24badb3662b79059258af127fcf77bf167c04bec6a",
-  "eth": "0x200184bde479ccffa50a89485417c907bc1d3779f9b1863d2158733135facce",
-  "orderbook": "0x65588d49953bbaf62d6b80b4c3fd4c9743ea987cf4a5a5b5e8070602086b03d"
-=======
   messaging:
-    "0x2140c668323b57d1454707c62fde8869600051056749be2b5c7369a3b5a3cfe",
-  executor: "0x2786451495b7e741a2dec0b816ddcea8f722f4d68127057100ea7173ecebf2c",
+    "0x6e40ed6d24f08a5ec9dc1017c5c6542a002c2d9c5dbed0e95ce930e9eb40d58",
+  executor: "0x5a9a84950d5413d00094e9e8eb674d8aae962fc9f607be6d2dd35e516d6b4e7",
   nftContract:
-    "0x60b89c9faec7ae27a95b4970ee2d578c96bcb08b782cc294829f4676f6dd3f7",
+    "0x87db65c66b914a83bbea24badb3662b79059258af127fcf77bf167c04bec6a",
   eth: "0x200184bde479ccffa50a89485417c907bc1d3779f9b1863d2158733135facce",
-  orderbook: "0x7dd5a2336f6a614ba8c110518e46d1a62e42fb173dfe835c8839a41c2d9f7dd"
->>>>>>> main
+  orderbook: "0x65588d49953bbaf62d6b80b4c3fd4c9743ea987cf4a5a5b5e8070602086b03d"
 };
