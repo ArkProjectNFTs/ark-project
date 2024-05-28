e30adfab10ae4ca288e264279ad8b44f4085efab
paul launay <launay.p@gmail.com>
2024-05-28 11:45:18 +0200

    refactor(core): remove unused util

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,7 +1,6 @@
 import * as starknet from "@scure/starknet";
 import { CallData } from "starknet";
 
-import { Config } from "../createConfig.js";
 import { OrderV1 } from "../types/index.js";
 
 export const getOrderHashFromOrderV1 = (order: OrderV1) => {
@@ -10,16 +9,4 @@ export const getOrderHashFromOrderV1 = (order: OrderV1) => {
   });
   const compiletOrderBigIntArray = compiledOrder.map(BigInt);
   return starknet.poseidonHashMany(compiletOrderBigIntArray);
-};
-
-export const getOrderbookAbi = async (config: Config) => {
-  // Retrieve the ABI for the order book contract
-  const { abi: orderbookAbi } = await config.arkProvider.getClassAt(
-    config.arkchainContracts.orderbook
-  );
-  if (orderbookAbi === undefined) {
-    throw new Error("no abi.");
-  } else {
-    return orderbookAbi;
-  }
 };
