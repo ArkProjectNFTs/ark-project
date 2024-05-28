c514ea011abb62de72c56e9dcee4af5198139253
paul launay <launay.p@gmail.com>
2024-05-28 15:48:29 +0200

    fix(apps/example): fix build issue

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -1,4 +1,6 @@
 // This file is auto-generated. Do not edit directly.
+/* eslint-disable */
+/* prettier-ignore */
 
 export const GOERLI_CONTRACTS = {
   nftContract:
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -46,4 +46,4 @@ export type {
   CreateConfigParameters
 } from "./createConfig.js";
 
-export { starknetRpcUrls, arkchainRpcUrls } from "./constants.js";
+export { starknetRpcUrls, arkchainRpcUrls, networks } from "./constants.js";
