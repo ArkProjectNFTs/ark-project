5a507552e180f479a8fdd80ee40f5ae4f881f94d
kwiss <kwiss@users.noreply.github.com>
2024-06-04 21:25:02 +0000

    chore: format

--- packages/core/src/contracts.ts
+++ packages/core/src/contracts.ts
@@ -2,8 +2,10 @@
 
 export const SEPOLIA_CONTRACTS = {};
 export const MAINNET_CONTRACTS = {
-  "nftContract": "0x32d99485b22f2e58c8a0206d3b3bb259997ff0db70cffd25585d7dd9a5b0546",
-  "messaging": "0x57d45cc46de463f7ae63b74ce9b6b6b496a1178b02e7ad04d7c307caa698b7b",
-  "executor": "0x7b42945bc47001db92fe1b9739d753925263f2f1036c2ae1f87536c916ee6a",
-  "orderbook": "0x51d61ee6b0814a990a82fa7c8d9515c19f82d4bdb34e35e3d00762caa752bdb"
+  nftContract:
+    "0x32d99485b22f2e58c8a0206d3b3bb259997ff0db70cffd25585d7dd9a5b0546",
+  messaging:
+    "0x57d45cc46de463f7ae63b74ce9b6b6b496a1178b02e7ad04d7c307caa698b7b",
+  executor: "0x7b42945bc47001db92fe1b9739d753925263f2f1036c2ae1f87536c916ee6a",
+  orderbook: "0x51d61ee6b0814a990a82fa7c8d9515c19f82d4bdb34e35e3d00762caa752bdb"
 };
