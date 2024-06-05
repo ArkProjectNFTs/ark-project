f072462f2f34071e61ff9c8686c14dd4b5a9d379
paul launay <launay.p@gmail.com>
2024-06-05 13:00:59 +0200

    feat(react): use approve multicall in auction hook

--- packages/react/src/hooks/useFulfillAuction.ts
+++ packages/react/src/hooks/useFulfillAuction.ts
@@ -2,31 +2,28 @@
 
 import { useState } from "react";
 
+import { AccountInterface } from "starknet";
+
 import { fulfillAuction } from "@ark-project/core";
 import { FulfillAuctionInfo } from "@ark-project/core/src/types";
 
 import { Status, StepStatus } from "../types";
-import { ApproveERC20Parameters, useApproveERC20 } from "./useApproveERC20";
-import { useBurnerWallet } from "./useBurnerWallet";
 import { useConfig } from "./useConfig";
-import { useOwner } from "./useOwner";
 
-export type fulfillAuctionParameters = ApproveERC20Parameters &
-  FulfillAuctionInfo;
+type ApproveERC20Parameters = {
+  starknetAccount: AccountInterface;
+  startAmount: bigint;
+  currencyAddress?: string;
+};
+
+export type FulfillAuctionParameters = FulfillAuctionInfo &
+  ApproveERC20Parameters;
 
 function useFulfillAuction() {
   const [status, setStatus] = useState<Status>("idle");
-  const [stepStatus, setStepStatus] = useState<StepStatus>("idle");
-  const { approveERC20, getAllowance } = useApproveERC20();
-  const owner = useOwner();
-  const arkAccount = useBurnerWallet();
   const config = useConfig();
 
-  async function fulfill(parameters: fulfillAuctionParameters) {
-    if (!arkAccount) {
-      throw new Error("No burner wallet.");
-    }
-
+  async function fulfill(parameters: FulfillAuctionParameters) {
     if (!config) {
       throw new Error("Invalid config.");
     }
@@ -34,25 +31,8 @@ function useFulfillAuction() {
     setStatus("loading");
 
     try {
-      const allowance = await getAllowance(
-        parameters.starknetAccount,
-        parameters.currencyAddress || config?.starknetContracts.eth
-      );
-
-      setStepStatus("approving");
-
-      await approveERC20({
-        starknetAccount: parameters.starknetAccount,
-        startAmount: Number(parameters.startAmount) + Number(allowance),
-        currencyAddress:
-          parameters.currencyAddress || config?.starknetContracts.eth
-      });
-
-      setStepStatus("selling");
-
       await fulfillAuction(config, {
         starknetAccount: parameters.starknetAccount,
-        arkAccount,
         fulfillAuctionInfo: {
           orderHash: parameters.orderHash,
           relatedOrderHash: parameters.relatedOrderHash,
@@ -60,19 +40,21 @@ function useFulfillAuction() {
           tokenId: parameters.tokenId,
           brokerId: parameters.brokerId
         },
-        owner
+        approveInfo: {
+          currencyAddress: (parameters.currencyAddress ||
+            config?.starknetCurrencyContract) as string,
+          amount: BigInt(parameters.startAmount)
+        }
       });
 
       setStatus("success");
-      setStepStatus("sold");
     } catch (error) {
       console.error(error);
       setStatus("error");
-      setStepStatus("error");
     }
   }
 
-  return { fulfill, status, stepStatus };
+  return { fulfill, status };
 }
 
 export { useFulfillAuction };
