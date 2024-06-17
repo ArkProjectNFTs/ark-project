2b855301c632d1e0f202a8c19e82b42c1a639ef3
kwiss <mr.dumont.christophe@gmail.com>
2024-06-17 13:20:02 +0200

    fix: update to clean scripts

--- examples/core/createBroker.ts
+++ examples/core/createBroker.ts
@@ -4,5 +4,4 @@ import { config } from "./config/index.js";
 
 const brokerID = process.env.BROKER_ID as string;
 console.log("Creating broker with ID: " + brokerID);
-console.log(config);
 createBroker(config, { brokerID });
