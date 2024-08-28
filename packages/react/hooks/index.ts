a5a022b45c25d5e724ade1966d696041148964a5
paul launay <launay.p@gmail.com>
2024-08-28 03:00:42 +0200

    feat(react): add typecheck

--- packages/react/hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as useCancel } from "./useCancel";
-export { default as useCreateListing } from "./useCreateListing";
-export { default as useCreateAuction } from "./useCreateAuction";
-export { default as useCreateOffer } from "./useCreateOffer";
-export { default as useFulfillListing } from "./useFulfillListing";
-export { default as useFulfillAuction } from "./useFulfillAuction";
-export { default as useFulfillOffer } from "./useFulfillOffer";
-export { useConfig } from "./useConfig";
