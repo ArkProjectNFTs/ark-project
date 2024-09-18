b5f1ba6339f9001368498dbfcc5eab1938ce53f0
paul launay <launay.p@gmail.com>
2024-09-18 20:15:27 +0200

    style(lint): replace eslint and prettier with biome

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -3,21 +3,21 @@
  * https://jestjs.io/docs/configuration
  */
 
-import type { Config } from "jest";
+import type { Config } from 'jest'
 
 const config: Config = {
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1"
+    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  preset: "ts-jest/presets/default-esm",
-  testMatch: ["**/?(*.)test.ts?(x)"],
+  preset: 'ts-jest/presets/default-esm',
+  testMatch: ['**/?(*.)test.ts?(x)'],
   globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json",
-      diagnostics: false
-    }
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+      diagnostics: false,
+    },
   },
-  setupFiles: ["<rootDir>/tests/setup.ts"]
-};
+  setupFiles: ['<rootDir>/tests/setup.ts'],
+}
 
-export default config;
+export default config
