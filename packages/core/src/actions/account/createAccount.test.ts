1bce14a9d679c5a5ade45f9ddde862d436f5f909
sanieni6 <luissanchezzamora23@gmail.com>
2024-10-01 15:49:26 -0500

    [refactor] move test from /test to /actions/account

---
+++ packages/core/src/actions/account/createAccount.test.ts
@@ -0,0 +1,17 @@
+import { createAccount } from "./account.js";
+import { config } from "../../../tests/utils/index.js";
+
+describe("createAccount", () => {
+  it("default", async () => {
+    const { arkProvider } = config;
+    const accountDetails = await createAccount(arkProvider);
+
+    expect(accountDetails).toHaveProperty("address");
+    expect(typeof accountDetails.address).toBe("string");
+    expect(accountDetails).toHaveProperty("privateKey");
+    expect(typeof accountDetails.privateKey).toBe("string");
+    expect(accountDetails).toHaveProperty("publicKey");
+    expect(typeof accountDetails.publicKey).toBe("string");
+    expect(accountDetails).toHaveProperty("account");
+  });
+});
