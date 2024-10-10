6d3184840567c5a8ac30fa0af69b1dbf27b74d4f
kwiss <mr.dumont.christophe@gmail.com>
2024-10-11 00:12:25 +0200

    fix: update contract address

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -27,11 +27,5 @@ export const starknetExecutorContracts = {
 export const starknetEthContract =
   "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
-export const arkchainOrderbookContracts = {
-  mainnet: MAINNET_CONTRACTS.orderbook,
-  sepolia: SEPOLIA_CONTRACTS.orderbook,
-  dev: ""
-};
-
 export const SOLIS_ACCOUNT_CLASS_HASH =
   "0x05400e90f7e0ae78bd02c77cd75527280470e2fe19c54970dd79dc37a9d3645c";
